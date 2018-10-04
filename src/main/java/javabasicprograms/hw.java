2384fc6b9a5ba2fdc303a7a745ce2b044c405a54
Aakash Muralidharan <m.aakash1701@gmail.com>
2018-10-04 15:19:53 +0530

    third commit aakash

--- src/main/java/javabasicprograms/hw.java
+++ src/main/java/javabasicprograms/hw.java
@@ -1,8 +1,30 @@
 package javabasicprograms;
 
+
+import java.util.Scanner;
+
 public class hw {
         public static void main(String[] args) {
             System.out.println("hello world");
+            Scanner in=new Scanner(System.in);
+            double si;
+            double n;
+            double r;
+            double p;
+            System.out.println("Enter P");
+            p=in.nextDouble();
+            System.out.println("Enter N");
+            n=in.nextDouble();
+            System.out.println("Enter R");
+            r=in.nextDouble();
+            si=p*n*r/100;
+            formula(si);
+
+
+        }
+        public static void formula(double si)
+        {
+            System.out.println("SI is "+si);
         }
 
 }
